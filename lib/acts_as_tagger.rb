18b412c0dc362646286cbabe26da3dbbc2112a8c
maxjustus <maxjustus@gmail.com>
2008-12-11 14:25:57 -0600

    made acts_as_tagger order tags the same way acts_as_taggable does

--- lib/acts_as_tagger.rb
+++ lib/acts_as_tagger.rb
@@ -8,7 +8,7 @@ module ActiveRecord
       module ClassMethods
         def acts_as_tagger(options = {})
           has_many :taggings
-          has_many :tags, :through => :taggings, :select => "DISTINCT #{Tag.table_name}.*", :order => "name"
+          has_many :tags, :through => :taggings, :select => "DISTINCT #{Tag.table_name}.*", :order => "lOWER(name) asc"
           
           extend ActiveRecord::Acts::Tagger::SingletonMethods
           include ActiveRecord::Acts::Tagger::InstanceMethods
