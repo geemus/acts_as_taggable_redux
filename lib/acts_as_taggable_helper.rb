88d85aa0865e2c29ccf24855f9cd02a37d6744fb
Akshay Shah <ashah@backchannelmedia.com>
2009-10-06 04:11:20 +0800

    fix sorting typo in helper

--- lib/acts_as_taggable_helper.rb
+++ lib/acts_as_taggable_helper.rb
@@ -3,7 +3,7 @@ module ActsAsTaggableHelper
   def link_to_tag(tag)
     link_to(tag.name, tag_url(tag), :rel => 'tag')
   end
-  
+
   # Generate a tag cloud of the top 100 tags by usage, uses the proposed hTagcloud microformat.
   #
   # Inspired by http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
@@ -12,24 +12,24 @@ module ActsAsTaggableHelper
     options.reverse_merge! :limit => 100, :sort => :name
     sort = options.delete(:sort)
 
-    tags = Tag.find(:all, options.merge(:order => 'taggings_count DESC')).sort_by(&sort)
-    
+    tags = Tag.find(:all, options.merge(:order => 'taggings_count DESC')).sort_by(&:sort)
+
     # TODO: add option to specify which classes you want and overide this if you want?
     classes = %w(popular v-popular vv-popular vvv-popular vvvv-popular)
-    
+
     max, min = 0, 0
     tags.each do |tag|
       max = tag.taggings_count if tag.taggings_count > max
       min = tag.taggings_count if tag.taggings_count < min
     end
-    
+
     divisor = ((max - min) / classes.size) + 1
-    
+
     html =    %(<div class="hTagcloud">\n)
     html <<   %(  <ul class="popularity">\n)
     tags.each do |tag|
       html << %(    <li>)
-      html << link_to(tag.name, tag_url(tag), :class => classes[(tag.taggings_count - min) / divisor]) 
+      html << link_to(tag.name, tag_url(tag), :class => classes[(tag.taggings_count - min) / divisor])
       html << %(</li> \n)
     end
     html <<   %(  </ul>\n)
